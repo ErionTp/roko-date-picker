2ddac155e1578aa21d25b045c9ec1541267ad252
Erjon Kelleci <kelleci.erion@gmail.com>
2023-12-03 14:49:41 +0100

    bug fix

--- src/features/hooks/useMonth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { MonthContext } from "../providers/month";
-
-export default function useMonth() {
-  const context = useContext(MonthContext);
-  if (!context) throw new Error("The useMain function ought to be encapsulated within the scope of the MainProvider component.");
-  return context;
-}
